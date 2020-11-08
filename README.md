f529c7a2c24ea8b48ae0f17cfdcc7f73f92d9110
PunkUnknown <PunkUnknown@tutanota.com>
2020-11-08 16:38:59 +0500

    finalized calculations

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# airdrop-list
+# Airdrop List
+### Provides token air drop calculations
+
+**Debase to airdrop: 60,000**
+
+These calculations are based upon three block chain snap shots.
+
+1. 11153477
+2. 11156973
+3. 11200637
+
+First debase holder balances are extracted using the graph.
+Second debase balances in the liquidity pools are extracts also using the graphs.
+
+**Snap Shot Weights**
+1. 11153477 - 70% (42000 Debase)
+2. 11156973 - 20% (12000 Debase)
+3. 11200637 - 10% (6000 Debase)
+
+Then rewards are scaled according to the % of debase held by each account multiplied by the weight for each snapshot
--- index.js
+++ index.js
@@ -7,58 +7,94 @@ try {
 	const file2 = fs.readFileSync('./holder-snap2.json', 'utf8');
 	const file3 = fs.readFileSync('./holder-snap3.json', 'utf8');
 
+	const file4 = fs.readFileSync('./liq-snap1.json', 'utf8');
+	const file5 = fs.readFileSync('./liq-snap2.json', 'utf8');
+	const file6 = fs.readFileSync('./liq-snap3.json', 'utf8');
+
 	// parse JSON string to JSON object
 	const data1 = JSON.parse(file1);
 	const data2 = JSON.parse(file2);
 	const data3 = JSON.parse(file3);
 
+	const data4 = JSON.parse(file4);
+	const data5 = JSON.parse(file5);
+	const data6 = JSON.parse(file6);
+
 	// print all databases
 
-	let snap1Supply = 70000;
-	let snap2Supply = 20000;
-	let snap3Supply = 10000;
+	let snap1Supply = 42000;
+	let snap2Supply = 12000;
+	let snap3Supply = 6000;
 
-	let balance1 = 40214.053700808756;
-	let balance2 = 45935.811860364876;
-	let balance3 = 75747.89080264188;
+	const balance1 = 51131.41977538134;
+	const balance2 = 53862.33471932199;
+	const balance3 = 96704.47641222835;
 
-	const formatedData1 = data1.users
+	const formattedData1 = data1.users
 		.flatMap((ele) => {
 			if (ele.balance === '0') {
 				return [];
 			}
-			const newBalance = parseFloat(formatEther(ele.balance)) / balance1 * snap1Supply;
+			let liqBalanceToAdd = 0;
+			data4.users.forEach((liqEle) => {
+				if (liqEle.address == ele.address) {
+					liqBalanceToAdd = liqEle.debaseBalance;
+				}
+			});
+
+			const newBalance =
+				(parseFloat(formatEther(ele.balance)) + parseFloat(formatEther(liqBalanceToAdd))) /
+				balance1 *
+				snap1Supply;
 			return [ { address: ele.address, balance: newBalance } ];
 		})
 		.sort((e1, e2) => e2.balance - e1.balance);
 
-	const formatedData2 = data2.users
+	const formattedData2 = data2.users
 		.flatMap((ele) => {
 			if (ele.balance === '0') {
 				return [];
 			}
-			const newBalance = parseFloat(formatEther(ele.balance)) / balance2 * snap2Supply;
+			let liqBalanceToAdd = 0;
+			data5.users.forEach((liqEle) => {
+				if (liqEle.address == ele.address) {
+					liqBalanceToAdd = liqEle.debaseBalance;
+				}
+			});
+			const newBalance =
+				(parseFloat(formatEther(ele.balance)) + parseFloat(formatEther(liqBalanceToAdd))) /
+				balance2 *
+				snap2Supply;
 			return [ { address: ele.address, balance: newBalance } ];
 		})
 		.sort((e1, e2) => e2.balance - e1.balance);
 
-	const formatedData3 = data3.users
+	const formattedData3 = data3.users
 		.flatMap((ele) => {
 			if (ele.balance === '0') {
 				return [];
 			}
-			const newBalance = parseFloat(formatEther(ele.balance)) / balance3 * snap3Supply;
+			let liqBalanceToAdd = 0;
+			data6.users.forEach((liqEle) => {
+				if (liqEle.address == ele.address) {
+					liqBalanceToAdd = liqEle.debaseBalance;
+				}
+			});
+			const newBalance =
+				(parseFloat(formatEther(ele.balance)) + parseFloat(formatEther(liqBalanceToAdd))) /
+				balance3 *
+				snap3Supply;
 			return [ { address: ele.address, balance: newBalance } ];
 		})
 		.sort((e1, e2) => e2.balance - e1.balance);
 
-	let data = JSON.stringify(formatedData1);
+	let data = JSON.stringify(formattedData1);
 	fs.writeFileSync('./snap1-air-drop.json', data);
 
-	data = JSON.stringify(formatedData2);
+	data = JSON.stringify(formattedData2);
 	fs.writeFileSync('./snap2-air-drop.json', data);
 
-	data = JSON.stringify(formatedData3);
+	data = JSON.stringify(formattedData3);
 	fs.writeFileSync('./snap3-air-drop.json', data);
 } catch (err) {
 	console.log(`Error reading file from disk: ${err}`);
